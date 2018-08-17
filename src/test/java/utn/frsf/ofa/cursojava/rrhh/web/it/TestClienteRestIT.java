7868de5a831e3236abcdbc42e95700f88bb2ab0b
mart-dominguez <mdomingu@gmail.com>
2018-08-17 11:32:36 -0300

    test api rest real

--- src/test/java/utn/frsf/ofa/cursojava/rrhh/web/it/TestClienteRestIT.java
+++ src/test/java/utn/frsf/ofa/cursojava/rrhh/web/it/TestClienteRestIT.java
@@ -5,6 +5,7 @@
  */
 package utn.frsf.ofa.cursojava.rrhh.web.it;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.io.BufferedReader;
@@ -24,9 +25,11 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.BeforeClass;
 import org.junit.Ignore;
 
 /**
@@ -35,51 +38,68 @@ import org.junit.Ignore;
  */
 public class TestClienteRestIT {
 
-    public TestClienteRestIT() {
-    }
-
-    @Before
-    public void setUp() {
-
-    }
-
-    @After
-    public void tearDown() {
-    }
-
-    @Test
+    @Ignore
     public void testGetCliente() throws IOException {
-        HttpGet httpget = new HttpGet("http://localhost:8080/rrhh-web/api/cliente");
+        String MY_URL = "http://localhost:8080/rrhh-web/api/cliente";
+        HttpGet httpget = new HttpGet(MY_URL);
         CloseableHttpClient cliente = HttpClients.createDefault();
         CloseableHttpResponse response1 = cliente.execute(httpget);
-        HttpEntity entity1 = response1.getEntity();            
+        HttpEntity entity1 = response1.getEntity();
         String resultado = entidadToString(entity1.getContent());
         assertEquals("GET", resultado.toUpperCase());
-        EntityUtils.consume(entity1);            
+        EntityUtils.consume(entity1);
         response1.close();
+
     }
-    
-    @Test
+
+    @Ignore
     public void testPostCliente() throws IOException {
-        HttpPost httpPost = new HttpPost("http://localhost:8080/rrhh-web/api/cliente");        
+        HttpPost httpPost = new HttpPost("http://localhost:8080/rrhh-web/api/cliente");
         ObjectMapper mapper = new ObjectMapper();
         ObjectNode clienteJson = mapper.createObjectNode();
         clienteJson.put("id", 1);
         clienteJson.put("nombre", "martin");
         clienteJson.put("correo", "mdo@mail.com");
         clienteJson.put("cuit", "203040");
-        
+
         StringEntity postingString = new StringEntity(clienteJson.toString());
         httpPost.setEntity(postingString);
         httpPost.setHeader("Content-type", "application/json");
-        
+
+    }
+
+    @Test
+    public void testCrearLuegoBuscarPorNombre() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode clienteJson = mapper.createObjectNode();
+        clienteJson.put("nombre", "martin999");
+        clienteJson.put("correo", "mdo@mail.com");
+        clienteJson.put("cuit", "203040");
+
+        StringEntity postingString = new StringEntity(clienteJson.toString());
+        HttpPost httpPost = new HttpPost("http://localhost:8080/rrhh-web/api/cliente/");
+        httpPost.setEntity(postingString);
+        httpPost.setHeader("Content-type", "application/json");
+
         CloseableHttpClient cliente = HttpClients.createDefault();
         CloseableHttpResponse response1 = cliente.execute(httpPost);
-        HttpEntity entity1 = response1.getEntity();            
-        String resultado = entidadToString(entity1.getContent());
-        assertEquals("POST", resultado.toUpperCase());
-        EntityUtils.consume(entity1);            
+        HttpEntity entity1 = response1.getEntity();
+        assertEquals(200, response1.getStatusLine().getStatusCode());
+        EntityUtils.consume(entity1);
         response1.close();
+
+        HttpGet httpget = new HttpGet("http://localhost:8080/rrhh-web/api/cliente/?nombre=martin999");
+        CloseableHttpResponse respBuscarPorNombre = cliente.execute(httpget);
+        HttpEntity clientePorNombre = respBuscarPorNombre.getEntity();
+        String resultado = entidadToString(clientePorNombre.getContent());
+        System.out.println("resultado: " + resultado);
+
+        // transformar el JSON en un objeto mapper
+        JsonNode clienteJsonRespuesta = mapper.readTree(resultado);        
+        assertEquals(clienteJsonRespuesta.get(0).get("nombre").asText().toLowerCase(), "martin999");
+        EntityUtils.consume(respBuscarPorNombre.getEntity());
+        response1.close();
+
     }
 
     private String entidadToString(InputStream is) throws IOException {
